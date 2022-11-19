e08f73af0fcca02ae95efd7704a508dfe1de6f78
Diego Veras <diego.veras96@hotmail.com>
2022-11-19 16:53:08 -0300

    Mudanças Último Dia
    Alteração na forma de pegar o última dia do mês e configuração de anos bissextos

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('.boxSearch').style.zIndex = "-1";
         dataCalendario.innerHTML = `${meses[mes]} ${ano}`;
         let firstDayOfWeek = new Date(ano, mes, 1).getDay() - 1; //pega o dia da semana que caiu o dia 1º
-        let getLastDayThisMonth = new Date(ano, mes + 1, 0).getDate();
+        let getLastDayThisMonth = diasMeses[mes];
+        if (mes==1 && ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0){ //Verificação de Ano Bissexto
+                getLastDayThisMonth = 29;
+        }
 
         //FOR para alterar os valores da tabela dos calendários para o dia certo do mês
         for (var i = -firstDayOfWeek, index = 0; i < (42 - firstDayOfWeek); i++, index++) {
@@ -16,18 +19,18 @@ document.addEventListener('DOMContentLoaded', function () {
             dayTable.classList.remove('proximoMes');
             dayTable.classList.remove('diaAtual');
             dayTable.innerHTML = dt.getDate(); //Altera o valor de acordo com o dia
-
             if (i < 1) {
                 dayTable.classList.add('mesAnterior');
             } else if (i > getLastDayThisMonth) {
                 dayTable.classList.add('proximoMes');
+
             } else if (dt.getFullYear() == tday.getFullYear() && dt.getMonth() == tday.getMonth() && dt.getDate() == tday.getDate()) {
                 dayTable.classList.add('diaAtual');
             }
         }
         for(i=0, index =0; i < 6; i++, index++){
-            dt = new Date(ano, mes, i)
-            dayTable = tableDays.getElementsByTagName('td')[index]
+            dt = new Date(ano, mes, i);
+            dayTable = tableDays.getElementsByTagName('td')[index];
         }
     }
 
@@ -36,7 +39,6 @@ document.addEventListener('DOMContentLoaded', function () {
         opcoes.classList.remove('clicado');
 
         let container = document.querySelector('.boxSearch');
-
         container.style.zIndex = "2";
         
         opcoes.addEventListener("click", (evento)=>{
@@ -51,10 +53,8 @@ document.addEventListener('DOMContentLoaded', function () {
         let container = document.querySelector('.boxSearch');
         
         opcoes.classList.add('clicado');
-
-        console.log(opcoes)
         escAno.classList.add('confirmacao');
-        escAno.innerHTML = `<input class = "anoEscolhido" type="number" placeholder = "2022" value=${ano}><input id="send" type="submit" value="Enviar">`
+        escAno.innerHTML = `<input class = "anoEscolhido" type="number" placeholder = "2022" value=${ano}><input id="send" type="submit" value="Enviar">`;
 
         container.appendChild(escAno);
         btn = document.querySelector('input#send');
@@ -65,11 +65,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 alert('Ano inválido');
                 return;
             }
+            console.log('asdasdasdasdd\n' + mes, ano)
             getDaysCalendar(mes, ano);
         }
     }
 
     const meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
+    const diasMeses = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     let mes = new Date().getMonth();
     let ano = new Date().getFullYear();
     anoMin = ano - 100;
@@ -93,7 +95,7 @@ document.addEventListener('DOMContentLoaded', function () {
             container.appendChild(boxSearch);
 
              let opcoes = document.getElementsByTagName('span')[i];
-             confirmarOpc(opcoes)  
+             confirmarOpc(opcoes);
         }
     }
 
@@ -103,24 +105,24 @@ document.addEventListener('DOMContentLoaded', function () {
         if (mes > 11) {
             mes = 0;
             ano++;
-            if(ano+100 >= anoMax){ //Verificação e definição da data limite do calendário
+            if(ano > anoMax){ //Verificação e definição da data limite do calendário
                 mes = 11;
                 ano = anoMax;
             }
         }
-        getDaysCalendar(mes, ano)
+        getDaysCalendar(mes, ano);
     }
     preview.onclick = function () {
         mes--;
         if (mes < 0) {
             mes = 11;
             ano--;
-            if(ano-100 <= anoMin){ //Verificação e definição da data limite do calendário
+            if(ano < anoMin){ //Verificação e definição da data limite do calendário
                 mes = 0;
-                ano = anoMin
+                ano = anoMin;
             }
         }
-        getDaysCalendar(mes, ano)
+        getDaysCalendar(mes, ano);
     }
 
     getDaysCalendar(mes, ano);
