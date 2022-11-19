9bc9ec4dca0e473975fa33e83d20450d150efebf
Diego Veras <diego.veras96@hotmail.com>
2022-11-19 11:36:30 -0300

    Melhorias e Correções
    Correções na pesquisa e melhorias nela

--- main.js
+++ main.js
@@ -32,32 +32,46 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function confirmarOpc(opcoes){
-        document.querySelector('.boxSearch').style.zIndex = "2";
+        opcoes.classList.remove('clicado');
+
+        let container = document.querySelector('.boxSearch');
+
+        container.style.zIndex = "2";
+        
         opcoes.addEventListener("click", (evento)=>{
             mes = evento.target.dataset.mes;
-            return escolherAno(mes);
+            return escolherAno(mes, opcoes);
         })
     }
 
-    function escolherAno(mes){
+    function escolherAno(mes, opcoes){
         let escAno = document.createElement('div');
         let container = document.querySelector('.boxSearch');
+        
+        opcoes.classList.add('clicado');
 
+        console.log(opcoes)
         escAno.classList.add('confirmacao');
         escAno.innerHTML = `<input class = "anoEscolhido" type="number" placeholder = "2022" value=${ano}><input id="send" type="submit" value="Enviar">`
 
         container.appendChild(escAno);
-        btn = document.querySelector('input#send')
+        btn = document.querySelector('input#send');
         btn.onclick = ()=>{
-            ano = document.querySelector('.anoEscolhido')
-            console.log(ano.value)
-            getDaysCalendar(mes, ano.value)
+            ano = document.querySelector('.anoEscolhido').value;
+
+            if (ano < anoMin || ano > anoMax){
+                alert('Ano inválido');
+                return;
+            }
+            getDaysCalendar(mes, ano);
         }
     }
 
     const meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
     let mes = new Date().getMonth();
     let ano = new Date().getFullYear();
+    anoMin = ano - 100;
+    anoMax = ano + 100;
     let calendario = document.querySelector('.calendario');
     let dataCalendario = document.querySelector('.mes');
     const tableDays = document.querySelector('.dias');
@@ -66,8 +80,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const next = document.getElementById('next');
 
     dataCalendario.onclick = ()=>{
-       
-        for (var i = 0; i<12; i++){
+        for (var i=0; i<12; i++){
             let boxSearch = document.createElement('div');
             let container = document.querySelector('.boxSearch');
 
@@ -87,6 +100,10 @@ document.addEventListener('DOMContentLoaded', function () {
         if (mes > 11) {
             mes = 0;
             ano++;
+            if(ano+100 >= anoMax){
+                mes = 11;
+                ano = anoMax;
+            }
         }
         getDaysCalendar(mes, ano)
     }
@@ -95,6 +112,10 @@ document.addEventListener('DOMContentLoaded', function () {
         if (mes < 0) {
             mes = 11;
             ano--;
+            if(ano-100 <= anoMin){
+                mes = 0;
+                ano = anoMin
+            }
         }
         getDaysCalendar(mes, ano)
     }
